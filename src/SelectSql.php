d06cd0cf36ed3f9c2cc39f4aa407370415855f6a
memran <memran.dhk@gmail.com>
2020-05-14 22:58:43 +0600

    fix offset sql string append missing

--- src/SelectSql.php
+++ src/SelectSql.php
@@ -8,6 +8,7 @@
  **/
 namespace MarwaDB;
 
+use Exception;
 use MarwaDB\WhereSqlTrait;
 use MarwaDB\Exceptions\ArrayNotFoundException;
 
@@ -15,36 +16,77 @@ class SelectSql
 {
 	use WhereSqlTrait;
 
-	var $sql_query='';
-
-		/**
+	/**
+	 * [$sql_query description]
+	 * @var string
+	 */
+	var $sql_query=NULL;
+	/**
 	 * var string table name
 	 * */
 	var $table;
 	/**
 	 * var object
 	 * */
-	var $db=null;
-
+	var $db=NULL;
 	/**
 	 * var string
 	 * */
-	var $offset=null;
-
+	var $offset=NULL;
 	/**
 	 * var array
 	 * */
 	var	$selectQuery = [];
+	/**
+	 * [$selectFormat description]
+	 * @var string
+	 */
 	var $selectFormat = 'SELECT %s FROM %s';
+	/**
+	 * [$distinctFormat description]
+	 * @var string
+	 */
 	var $distinctFormat = 'SELECT DISTINCT %s FROM %s';
-	var $whereSql=null;  //where sql
-	var $whereOrSql=null; //where OR sql
-	var $whereAndSql=null; //where AND sql
-	var $limitSql= null; //limit sql
-	var $orderSql = null; //Order By sql
-	var $groupSql = null; //GROUP BY sql
-	var $havingSql = null; //Having SQL
-	var $joinSql = null;
+	/**
+	 * [$whereSql description]
+	 * @var null
+	 */
+	var $whereSql=NULL;  //where sql
+	/**
+	 * [$whereOrSql description] where OR sql
+	 * @var null
+	 */
+	var $whereOrSql=NULL;
+	/**
+	 * [$whereAndSql description] where AND sql
+	 * @var null
+	 */
+	var $whereAndSql=NULL;
+	/**
+	 * [$limitSql description] limit sql
+	 * @var null
+	 */
+	var $limitSql= NULL;
+	/**
+	 * [$orderSql description] Order By sql
+	 * @var null
+	 */
+	var $orderSql = NULL;
+	/**
+	 * [$groupSql description] GROUP BY sql
+	 * @var null
+	 */
+	var $groupSql = NULL;
+	/**
+	 * [$havingSql description] Having SQL
+	 * @var null
+	 */
+	var $havingSql = NULL;
+	/**
+	 * [$joinSql description]
+	 * @var null
+	 */
+	var $joinSql = NULL;
 
 	/**
 	 * function __construct
@@ -117,25 +159,33 @@ class SelectSql
 		return $this;
 	}
 
-
 	/**
 	 * function to get Sql as string without execute
 	 * @return  string description
 	 * */
 	public function sqlString()
 	{
-		$sql=null;
-		$result=null;
+		//define variable for local sql string
+		$selectColumn=NULL;
+
+		//if select query is not empty
 		if(!empty($this->selectQuery))
 		{
-			$sql = implode(',',$this->selectQuery);
+			if(is_array($this->selectQuery))
+			{
+				$selectColumn = implode(',',$this->selectQuery);
+			}
+			else
+			{
+				$selectColumn = $this->selectQuery;
+			}
 		}
 		else
 		{
-			$sql = "*";
+			$selectColumn = "*";
 		}
-
-		 $this->sql_query = sprintf($this->selectFormat,$sql,$this->table);
+		//build sql query
+		 $this->sql_query = sprintf($this->selectFormat,$selectColumn,$this->table);
 
 		 //check join/leftjoin/rightjoin is null or not
 		 if(!is_null($this->joinSql))
@@ -151,14 +201,14 @@ class SelectSql
 
   		 	//check if not null whereOrSql
   		 	if(!is_null($this->whereOrSql))
-  			 {
-  			 	    $this->sql_query .= $this->whereOrSql;
-  			 }
-			 //check if not null whereAndSql
-			 if(!is_null($this->whereAndSql))
-			 {
-			 	    $this->sql_query .= $this->whereAndSql;
-			 }
+  			{
+  				$this->sql_query .= $this->whereOrSql;
+  			}
+			//check if not null whereAndSql
+			if(!is_null($this->whereAndSql))
+			{
+			    $this->sql_query .= $this->whereAndSql;
+			}
 	    }
 
 		 //checking if not null havingSql
@@ -183,7 +233,11 @@ class SelectSql
 		 {
 		 	$this->sql_query .= $this->limitSql;
 		 }
-
+		 //check offset
+		 if(!is_null($this->offset))
+		 {
+		 	$this->sql_query .= $this->offset;
+		 }
 		 return $this->sql_query;
 
 	}
@@ -194,18 +248,16 @@ class SelectSql
 	 * */
 	public function get()
 	{
-
+		//build sql query
 		$this->sqlString();
-
 	    //if Database object is not null then execute query
 		if(!is_null($this->db))
 		{
-  			$result = $this->db->rawQuery($this->sql_query,$this->placeHolders);
-			return $result;
+  			return $this->db->rawQuery($this->sql_query,$this->placeHolders);
 		}
 		else
 		{
-			return $this->sql_query;
+			throw new Exception('Database connection not found!');
 		}
 
 	}
@@ -236,12 +288,11 @@ class SelectSql
   public function havingRaw(string $columns="*")
 	{
 		//check if columns is null
-		if(is_null($columns) OR is_array($columns))
+		if(!is_string($columns))
 		{
 			throw new Exception("Parameter must be string");
 		}
-		$whereFormat=' HAVING %s';
-		$this->havingSql = sprintf($whereFormat,$columns);
+		$this->havingSql = sprintf(' HAVING %s',$columns);
 		return $this;
 	}
 
@@ -253,27 +304,27 @@ class SelectSql
 	public function orderBy(string $colName,$sortBy='ASC')
 	{
 		//check if colName is null
-		if(is_null($colName))
+		if(!is_string($colName))
 		{
 			throw new Exception("Orderby column name must be string");
 		}
 
 		$sortArray=['ASC','DESC','RAND'];
-    $colSql ='';
 
 		if(!in_array($sortBy,$sortArray))
 		{
 			throw new Exception("Sort By key is not valid");
 		}
-
-    if (strpos($colName, '.') !== false) {
-    	$colSql="{$colName} {$sortBy}";
-    }
-    else //add the table name
-    {
-      $colSql=$this->table.".{$colName} {$sortBy}";
-    }
-		$this->orderByRaw($colSql);
+		//$colSql ='';
+	    if (strpos($colName, '.') !== false)
+	    {
+	    	$this->orderByRaw("{$colName} {$sortBy}");
+	    }
+	    else //add the table name
+	    {
+	      $this->orderByRaw($this->table.".{$colName} {$sortBy}");
+	    }
+		//$this->orderByRaw($colSql);
 		return $this;
 	}
 
@@ -285,18 +336,18 @@ class SelectSql
 	public function groupBy(string $colName)
 	{
 		//check if colName is null
-		if(is_null($colName))
+		if(!is_string($colName))
 		{
 			throw new Exception("Parameter must be string");
 		}
 
-    if (strpos($colName, '.') !== false) {
-    	$this->groupSql = " GROUP BY {$colName}";
-    }
-    else //add the table name
-    {
-      $this->groupSql = " GROUP BY {$this->table}.{$colName}";
-    }
+	    if (strpos($colName, '.') !== false) {
+	    	$this->groupSql = " GROUP BY {$colName}";
+	    }
+	    else //add the table name
+	    {
+	      $this->groupSql = " GROUP BY {$this->table}.{$colName}";
+	    }
 
 		return $this;
 	}
@@ -313,25 +364,24 @@ class SelectSql
 	{
 		//INNER JOIN Customers ON Orders.CustomerID=Customers.CustomerID;
 		$joinFormat=" {$type} JOIN %s ON %s %s %s";
+	    //check local table has dot or not
+	    if (strpos($local, '.') !== false) {
+	      $localCol = $local;
+	    }
+	    else //add the table name
+	    {
+	        $localCol = "{$this->table}.{$local}";
+	    }
 
-    //check local table has dot or not
-    if (strpos($local, '.') !== false) {
-      $localCol = $local;
-    }
-    else //add the table name
-    {
-        $localCol = "{$this->table}.{$local}";
-    }
-
-    //check join column has dot or not
-    if (strpos($joinCol, '.') !== false) {
-      $joinRef = $joinCol;
-    }
-    else //add the table name
-    {
-        $joinRef = "{$jointable}.{$joinCol}";
-    }
-		$this->joinSql .= sprintf($joinFormat,$jointable,$joinRef,$operator,$local);
+	    //check join column has dot or not
+	    if (strpos($joinCol, '.') !== false) {
+	      $joinRef = $joinCol;
+	    }
+	    else //add the table name
+	    {
+	        $joinRef = "{$jointable}.{$joinCol}";
+	    }
+		$this->joinSql .= sprintf($joinFormat,$jointable,$joinRef,$operator,$localCol);
 		return $this;
 	}
 	/**
@@ -378,7 +428,6 @@ class SelectSql
 		{
 			$this->offset = " OFFSET {$offset}";
 		}
-
 		return $this;
 	}
 
@@ -387,13 +436,11 @@ class SelectSql
 	 * @param   $limit description
 	 * @return  $this description
 	 * */
-	public function limit($limit=50)
+	public function limit($limit=25)
 	{
-		$this->limitSql = " LIMIT {$limit}";
-		if(!is_null($this->offset))
+		if(!is_null($limit))
 		{
-			$this->limitSql.=$this->offset;
-
+			$this->limitSql = " LIMIT {$limit}";
 		}
 		return $this;
 	}
